200bfc8ab8e9e922a1e9ed49b0a505b5b55fee1a
aakarsh-2004 <aakarshbeohar2004@gmail.com>
2023-07-03 14:21:17 +0530

    Worked on card animations

--- card-animations/index.js
+++ card-animations/index.js
@@ -1,4 +1,21 @@
-document.querySelector('.card').addEventListener('mouseover', () => {
-    const targetElement = document.querySelector('.link-text');
-    targetElement.classList.add('link-text');
-})
+const targetElement = document.querySelector('.link-text');
+const cardComponent = document.querySelector('.card');
+const imgComponent = document.querySelector('.image');
+
+targetElement.style.visibility = 'hidden';
+
+cardComponent.addEventListener('mouseover', () => {
+    targetElement.style.visibility = 'visible';
+});
+
+cardComponent.addEventListener('mouseout', () => {
+    targetElement.style.visibility = 'hidden';
+});
+
+targetElement.addEventListener('mouseover', () => {
+    targetElement.style.visibility = 'visible';
+    imgComponent.classList.add('changed-img');
+});
+targetElement.addEventListener('mouseout', () => {
+    imgComponent.classList.remove('changed-img');
+});
